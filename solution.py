78ed06eac14a1bd81481d89e616c12ce2ece7950
Aleksandr Karpukhin <116599912+AleksandrK86@users.noreply.github.com>
2023-04-05 13:38:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1105105523 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    array=x
+    x_func=array-963
+    x_func=np.log2(x_func)
+    mu=np.mean(x_func)
+    sigma=np.std(x_func)
+    result = np.exp(mu + sigma**2 / 2.0)
+    return result # Ваш ответ
