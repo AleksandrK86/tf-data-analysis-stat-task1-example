f200bad0f23ef365f00f5bb5266bceb19f8ddb9f
Aleksandr Karpukhin <116599912+AleksandrK86@users.noreply.github.com>
2023-04-05 19:26:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,4 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    array=x
-    x_func=array-963
-    x_func=np.log2(x_func)
-    mu=np.mean(x_func)
-    sigma=np.std(x_func)
-    result = np.exp(mu + sigma**2 / 2.0)
-    return result # Ваш ответ
+    return np.log(x-963).mean() # Ваш ответ
