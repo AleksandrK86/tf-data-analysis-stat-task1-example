392903ec47ce4799f270d11bfd4d6f294740ff7e
Aleksandr Karpukhin <116599912+AleksandrK86@users.noreply.github.com>
2023-04-07 22:18:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,4 @@ import numpy as np
 chat_id = 1105105523 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return np.log(x-963).mean() # Ваш ответ
+    return np.log(x-963).mean()
